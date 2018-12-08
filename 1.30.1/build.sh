6e69ebb63b8f156084a9d7ae6675b897030f3019
Michael Neumann <mneumann@ntecs.de>
2018-12-08 13:50:37 +0100

    Build 1.30.1

---
+++ 1.30.1/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+RUSTC_BOOTSTRAP_VERSION=1.29.0
+CARGO_BOOTSTRAP_VERSION=0.30.0
+RUST_VERSION=1.30.1
+
+BASE=`pwd`
+DEST=$1
+LLVM_ROOT="/usr/local/llvm60"
+
+. ../checksums.sh
+. ../common.sh
+
+RUN info clean extract prepatch config RUN xbuild xdist inst 2>&1
