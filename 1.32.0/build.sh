39d64a0a5906b4f1ff6f6e6eae77e5872abf7707
Michael Neumann <mneumann@ntecs.de>
2019-03-02 18:00:50 +0100

    Add files for 1.32.0 (not tested)

---
+++ 1.32.0/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+RUSTC_BOOTSTRAP_VERSION=1.31.0
+CARGO_BOOTSTRAP_VERSION=0.32.0
+RUST_VERSION=1.32.0
+
+CONFIGURE_CARGO_STATIC_FLAGS="--enable-cargo-native-static"
+
+# XXX: miri does not build correctly that's why we need the flag below.
+ADDITIONAL_CONFIGURE_FLAGS="--enable-missing-tools"
+
+BASE=`pwd`
+DEST=$1
+LLVM_ROOT=""
+
+. ../checksums.sh
+. ../common.sh
+
+RUN info clean extract prepatch config xbuild xdist inst 2>&1
