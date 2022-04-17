e0dcb804ca8a4143914b789bf457dd341e23aa1d
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2022-04-17 13:03:28 +0200

    Build 1.56.0

---
+++ 1.56.0/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+RUSTC_BOOTSTRAP_VERSION=1.55.0
+CARGO_BOOTSTRAP_VERSION=1.55.0
+RUST_VERSION=1.56.0
+
+CONFIGURE_CARGO_STATIC_FLAGS="--enable-cargo-native-static"
+
+# XXX: miri does not build correctly that's why we need the flag below.
+ADDITIONAL_CONFIGURE_FLAGS="--enable-missing-tools"
+
+# Since rust 1.38, OPENSSL_DIR has to be specified.
+export OPENSSL_DIR="/usr/local"
+
+BASE=`pwd`
+DEST=$1
+LLVM_ROOT=""
+
+. ../checksums.sh
+. ../common.sh
+
+fixup-vendor() {
+	fixup-vendor-patch openssl build.rs || exit 1
+	fixup-vendor-patch openssl-sys build/cfgs.rs || exit 1
+}
+
+RUN info clean extract prepatch fixup-vendor config xbuild xdist inst 2>&1
