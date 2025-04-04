c5437ec806d2e2b1b2734dc73bde104ff66fedec
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2025-04-04 19:27:10 +0200

    Build 1.80.1

---
+++ 1.80.1/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+RUSTC_BOOTSTRAP_VERSION=1.79.0
+CARGO_BOOTSTRAP_VERSION=1.79.0
+RUST_VERSION=1.80.1
+
+CONFIGURE_CARGO_STATIC_FLAGS="--enable-cargo-native-static"
+
+# Since rust 1.38, OPENSSL_DIR has to be specified.
+export OPENSSL_DIR="/usr/local"
+
+# Show backtraces on failures
+export RUST_BACKTRACE=1
+
+BASE=`pwd`
+DEST=$1
+LLVM_ROOT=""
+
+. ../checksums.sh
+. ../common.sh
+
+fixup-vendor() {
+	# add here your vendor patches, example:
+	#fixup-vendor-patch openssl build.rs || exit 1
+}
+
+RUN info clean extract prepatch fixup-vendor config xbuild xdist inst 2>&1
